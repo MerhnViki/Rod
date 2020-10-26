acaee64866aae082bec97ded551d2b3907956536
Artorius <ssnegow@gmail.com>
2020-10-27 01:45:47 +0300

    App init #2

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ bootstrap = Bootstrap()
 moment = Moment()
 db = SQLAlchemy()
 
+
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
@@ -17,5 +18,8 @@ def create_app(config_name):
     bootstrap.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+
+    from . import main as main_blueprint
+    app.register_blueprint(main_blueprint.main)
 
     return app
---
+++ app/main/__init__.py
@@ -0,0 +1,3 @@
+from flask import Blueprint
+main = Blueprint('main_bp', __name__)
+from . import views, errors
